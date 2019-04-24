123fce396ace693c4690eaa11b13fcfe64f3967f
viczc <viczc@hotmail.com>
2019-04-24 21:30:29 +0800

    first commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'yd631xiaoshuo';
+$cfg_dbuser = 'yd631xiaoshuo';
+$cfg_dbpwd = 'cxL686n6aLuB5Svw';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
